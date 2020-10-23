513af4717f53b48e24b76ff2040af791fd1402e9
PMasters209 <73200873+PMasters209@users.noreply.github.com>
2020-10-23 11:12:01 -0500

    Add files via upload

--- GroupProjectMedicaid.py
+++ GroupProjectMedicaid.py
@@ -14,18 +14,33 @@ drugs = pd.read_csv('./product.csv')
 
 ##records = records.replace('UNKNOWN', value = np.nan)
 
+def Stringify(x):
+    realrec[x]= realrec[x].map(str)
+    realrec[x]= realrec[x].apply(lambda x:x.zfill(4))
+  
+
+#Get rid of the totaling columns and prescription numbers with no values:
 realrec = records[records['Number of Prescriptions'].notna()]
 realrec = realrec.replace('XX', value = np.nan)
 realrec = realrec[realrec['State'].notna()]
+
+#add a cost (for Medicaid) per unit column
 realrec['Cost per Unit'] = realrec['Medicaid Amount Reimbursed'] / realrec['Units Reimbursed']
+
+
+# If we want to know more about the drugs, we need to integrate the drug data and need Product NDC, 
+# which requires a minimum of 4 values in each field, zfill takes it to that minimum
+Stringify('Labeler Code')
+Stringify('Product Code')
+realrec['Product NDC'] = realrec['Labeler Code'] + '-' + realrec['Product Code'] 
 realrec.to_csv('CleanedRecords.csv', index = True, header = True)
 
-#Which Drug is most prescribed?
+#Which drug is most prescribed?
 grouped = realrec.groupby('Product Name').agg({'Units Reimbursed' : 'sum', 'Number of Prescriptions': 'sum' , 'Medicaid Amount Reimbursed' : 'mean', 'Non Medicaid Amount Reimbursed' : 'mean', 'Cost per Unit' : 'mean'})
 sortbyDrug = grouped.sort_values(by=['Number of Prescriptions'], ascending = False)
 sortbyDrug.to_csv('sortbyDrug.csv',index = True, header = True)
 
-#Which drug is most expensive?
+#Which drug is most expensive per unit?
 grouped = realrec.groupby('Product Name').agg({'Units Reimbursed' : 'sum', 'Number of Prescriptions': 'sum' , 'Medicaid Amount Reimbursed' : 'mean', 'Non Medicaid Amount Reimbursed' : 'mean', 'Cost per Unit' : 'mean'})
 sortbyExpense = grouped.sort_values(by=['Cost per Unit'], ascending = False)
 
